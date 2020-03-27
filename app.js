4366d75a2338262fe4bce80ed5be2db3735fb6b7
Roberto Alves Silva <8140410@estg.ipp.pt>
2020-03-27 18:31:52 +0000

    Segundo commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const openweathermaps_key = "YOUR_KEY_HERE"
+const openweathermaps_key = "5fc119390310fa925f5385956d31c26a"
 
-function searchWeather(lat, long) {
-    var lat = document.getElementById('paw-form-lat').value;
-    var lon = document.getElementById('paw-form-lon').value;
+function searchWeather() {
+    var lat = $("#paw-form-lat").val() //document.getElementById('paw-form-lat').value;
+    var lon = $("#paw-form-lon").val()  //document.getElementById('paw-form-lon').value;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -16,4 +16,15 @@ function searchWeather(lat, long) {
     xhttp.open("GET", `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${openweathermaps_key}`, true);
     xhttp.setRequestHeader('Accept', 'application/json');
     xhttp.send();
+    storeNumbers()
 }
+
+function storeNumbers(){
+    localStorage.setItem("lat", $("#paw-form-lat").val())
+    localStorage.setItem("lon", $("#paw-form-lon").val())
+}
+
+document.getElementById('paw-form-lat').value = localStorage.getItem("lat")
+document.getElementById('paw-form-lon').value = localStorage.getItem("lon")
+
+$("#paw-form-searchWeather").click(searchWeather)
